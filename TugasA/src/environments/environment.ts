f680a28398b0d0936df43045cd153c701dc40568
firmangaluhs <firmangaluh66@gmail.com>
2020-07-04 12:53:46 +0700

    Tugas UAS Bagian A

---
+++ TugasA/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDwo-q3DTupaFA1laej06-TXutCuL1eKgk",
+    authDomain: "memo-c3971.firebaseapp.com",
+    databaseURL: "https://memo-c3971.firebaseio.com",
+    projectId: "memo-c3971",
+    storageBucket: "memo-c3971.appspot.com",
+    messagingSenderId: "555401569368",
+    appId: "1:555401569368:web:b77995bd7817747d14562d"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
